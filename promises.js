ec9416c124e57d883b1aef002eb4faa3c0799ac8
Cole Hart <cole@colehart.com>
2018-11-25 22:39:14 -0700

    Remove console.log and add commented out Questions/answers

--- promises.js
+++ promises.js
@@ -1,3 +1,8 @@
+// Promises Prework
+// https://gist.github.com/robbiejaeger/dc8f55c1f9462741090862f736b82cab
+
+// Exercises
+
 const testNum = (number) => {
   return new Promise((resolve, reject) => {
     if( number > 10 ) resolve( number + ' is greater than 10, success!' )
@@ -22,7 +27,20 @@ const makeAllCaps = (list) => {
 
 const sortWords = (list) => {
   return new Promise((resolve, reject) => {
-    console.log(list);
     resolve(list.sort())
   })
 }
+
+/*
+Questions
+
+1. What is .then() used for, and what is .catch() used for?
+.then() awaits the resolution of a Promise before executing its callback function, while .catch() awaits the rejection (if any) of a Promise before exectuting its callback function
+
+2. What are good use cases for Promises?
+Any time you need to await the completion of something asyncronous before doing something else or store the result of an asyncronous operation for later use... API and database calls
+Google recommends using promises over callbacks because of the efficiency of the error handling: https://developers.google.com/api-client-library/javascript/features/promises
+
+3. What other libraries/functions can you find that use Promises?
+  Bluebird, fetch, jQuery, Q, WhenJS
+*/
